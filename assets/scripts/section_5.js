e07feb7070c715e6e3c63732db643f705cddfe07
Tan ix <oysteinrostvik@protonmail.com>
2024-05-09 09:24:07 +0200

    remove commented out code

--- assets/scripts/section_5.js
+++ assets/scripts/section_5.js
@@ -1,78 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
-/*document.addEventListener('DOMContentLoaded', () => {
-    gsap.from('.hookFish__fish2', {
-        /*scrollTrigger: {
-            trigger: '#section-5',
-            toggleActions: 'restart none none none',
-            start: 'top',
-            markers: true,
-        },
-        duration: 3,
-        x: '500%',
-        y: '100',
-        ease: 'power2',
-    });
-    gsap.to('.hookFish__fish2', {
-        duration: 0.6,
-        y: '-10%',
-        ease: 'power2',
-    }).delay(3);
-    gsap.to('.hookFish__fish2', {
-        duration: 0.6,
-        rotation: 85,
-        transformOrigin: 'left 50%',
-    }).delay(3);
-    gsap.to('.hookFish__fishHookWorm', {
-        /*scrollTrigger: {
-            trigger: '#section-5',
-            toggleActions: 'restart none none none',
-            start: 'top bottom',
-            markers: true,
-        },
-        duration: 1.6,
-        y: '-100%',
-        ease: 'power2',
-    }).delay(3.1);
-    gsap.to('.hookFish__fish2', {
-        /*scrollTrigger: {
-            trigger: '#section-5',
-            toggleActions: 'restart none none none',
-            start: 'top bottom',
-            markers: true,
-        },
-        duration: 3,
-        y: '-320%',
-        ease: 'power2',
-    }).delay(3.1);
-    gsap.to('.hookFish__fishHookWorm', {
-        /*scrollTrigger: {
-            trigger: '#section-5',
-            toggleActions: 'restart none none none',
-            start: 'top bottom',
-            markers: true,
-        },
-        duration: 3,
-        y: '0',
-        ease: 'power2',
-    }).delay(7);
-});*/
-
 const fishing_timeline = gsap.timeline({
     repeat: -1,
-    /*defaults: {
-        toggleActions: 'restart none none none',
-    },
-    scrollTrigger: {
-        trigger: '#section-5',
-        start: 'top top',
-        end: '200% bottom',
-        markers: true,
-        onEnter: () => console.log('enter'),
-        onLeave: () => console.log('leave'),
-        onEnterBack: () => console.log('enter back'),
-        onLeaveBack: () => console.log('enter all the way back'),
-    },*/
 });
 fishing_timeline
     .from('.hookFish__fish2', {
@@ -118,11 +47,3 @@ fishing_timeline
         y: '0',
         ease: 'power2',
     });
-
-/*let options = {
-        root: document.querySelector("#section5"),
-        rootMargin: "0px",
-        threshold: 9.0,
-      };
-      
-      let observer = new IntersectionObserver(callback, options);*/
